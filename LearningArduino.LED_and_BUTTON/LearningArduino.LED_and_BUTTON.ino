024f444c6849df254bb1a1403ab8a33291737cd4
Jason O'Toole <jason.otoole3@education.nsw.gov.au>
2024-06-06 09:01:35 +1000

    Completed LEDS & BUTTONS

--- LearningArduino.LED_and_BUTTON/LearningArduino.LED_and_BUTTON.ino
+++ LearningArduino.LED_and_BUTTON/LearningArduino.LED_and_BUTTON.ino
@@ -1,9 +1,37 @@
+#include "Led.h"
+#include "PushButton.h"
+
+#define RED_LED_PIN 11
+#define YELLOW_LED_PIN 10
+#define GREEN_LED_PIN 9
+#define BUTTON_PIN 2
+
+Led redLed(RED_LED_PIN);
+Led yellowLed(YELLOW_LED_PIN);
+Led greenLed(GREEN_LED_PIN);
+PushButton button(BUTTON_PIN, true, true);
+
 void setup() {
-  // put your setup code here, to run once:
+  redLed.init();
+  yellowLed.init();
+  greenLed.init();
+  button.init();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (button.isPressed()) {
+    redLed.on();
+    yellowLed.off();
+    greenLed.on();
+  }
+  else {
+    redLed.off();
+    yellowLed.on();
+    greenLed.off();
+  }
 
 }
+
+
+
